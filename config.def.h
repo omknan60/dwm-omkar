41f580040cf609757ec449a476926cbded446ad4
omkarNandan <omknan60@gmail.com>
2022-10-08 00:17:10 +0530

    lets see ....ummmm ... i dont remember

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Mensch:size=10",  "DejaVuSansMono Nerd Font Mono:size=13:antialias=true" };
-static const char dmenufont[]       =  "Mensch:size=10";
+static const char *fonts[]          = { "Mensch:size=11",  "DejaVuSansMono Nerd Font Mono:size=15:antialias=true" };
+static const char dmenufont[]       =  "Mensch:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
